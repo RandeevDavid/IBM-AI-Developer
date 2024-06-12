3d471298b8ce338f0b072936327463cbf54e4205
Randeev David <davidrandeev@gmail.com>
2024-06-12 12:12:36 -0400

    Image captioning with the BLIP model (Hugging face transformers library)

---
+++ image-captioning/image_cap.py
@@ -0,0 +1,24 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Load your image, DONT FORGET TO WRITE YOUR IMAGE NAME
+img_path = "dice.jpg"
+# convert it into an RGB format 
+image = Image.open(img_path).convert('RGB')
+
+# You do not need a question for image captioning (A question can be included here for visual question answering or VQA)
+text = "Image of"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length=200)
+
+# Decode the generated tokens to text
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+# Print the caption
+print(caption)
