f3cc39aceaa0e16f11bb81c72f4e7a1a1dbe5f96
Randeev David <davidrandeev@gmail.com>
2024-07-10 12:18:51 -0400

    Added code for using HuggingFace API to worker.py

--- pdf-summary-chatbot/worker.py
+++ pdf-summary-chatbot/worker.py
@@ -53,11 +53,14 @@ def init_llm():
     llm_hub = WatsonxLLM(model=LLAMA2_model)
 
     ### --> if you are using huggingFace API:
-    # Set up the environment variable for HuggingFace and initialize the desired model, and load the model into the HuggingFaceHub
-    # dont forget to remove llm_hub for watsonX
-    # os.environ["HUGGINGFACEHUB_API_TOKEN"] = "YOUR API KEY"
-    # model_id = "tiiuae/falcon-7b-instruct"
-    # llm_hub = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 600, "max_length": 600})
+    """
+    # Set up the environment variable for HuggingFace and initialize the desired model.
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "YOUR API KEY" # Need to get an API key
+    # Repo name for the model
+    model_id = "tiiuae/falcon-7b-instruct"
+    # Load the model into the HuggingFaceHub
+    llm_hub = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 600, "max_length": 600})
+    """
 
     # Initialize embeddings using a pre-trained model to represent the text data.
     # Create object of Hugging Face Instruct Embeddings with (model_name,  model_kwargs={"device": DEVICE} )
